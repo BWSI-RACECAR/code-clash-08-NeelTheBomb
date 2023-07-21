b8769d5eba396e6b623bb57e89c869449cefd80b
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-21 09:12:45 -0400

    Create optimizescan.py

---
+++ optimizescan.py
@@ -0,0 +1,19 @@
+class Solution:    
+    def optimizescan(self, battcap):
+        # Input type: Integer
+        # return type: float
+
+        #TODO: Write code below to return a float with the solution to the prompt.
+        return battcap/250
+
+        pass 
+
+def main():
+    battcap = int(input())
+
+    tc1 = Solution()
+    ans = tc1.optimizescan(battcap)
+    print("%.2f" % ans)
+
+if __name__ and "__main__":
+    main()
